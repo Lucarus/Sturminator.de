c34444e446347a97510a3bf3a82dad8ac508481f
Lukas <lukas_sturm@yahoo.de>
2017-02-11 01:09:06 +0100

    Added Comments

--- Sort/Controller.js
+++ Sort/Controller.js
@@ -35,13 +35,13 @@ function Controller() {
     }
 
     this.mouseMove = function(mouseX, mouseY) {
-
         // Wenn ein element geklickt wurde mit dem Mauszeiger verschieben
         if (this.liftedItemIndex != -1) {
             this.sortElements[this.liftedItemIndex].location = {X: mouseX - this.sortElements[this.liftedItemIndex].form.W * 0.5, Y: mouseY - this.sortElements[this.liftedItemIndex].form.H * 0.5};
         }
     }
 
+    //Variablen für Sortanimation
     this.isBubblesorting = false;
     this.bubbleSort_i = 1;
     this.bubbleSort_x = 0;
@@ -51,31 +51,43 @@ function Controller() {
     // Alle Sortieritems updaten lassen
     this.update = function() {
 
-        // anderes Zeig Animationen etc...
+        // Bubblesort Sortanimation
+        /*
+            Um mit den Animationen leichter arbeiten zu können wird die Sortfunktion in die Updateschleife ausgelagert.
+            Das heißt es wird ~ jede Sekunde ein schritt in dem Algorithmus durchlaufen.
+        */
         if (this.isBubblesorting) {
+            // Mainschleife nur ausführen, wenn wir gerade NICHT in einer Animationsphase sind
             if (this.nextSteppIn <= 0) {
+                //Äußere Schleife des Bubblesorts
                 if (this.bubbleSort_x < this.sortElements.length) {
+                    //Innere Schleife des Bubblesorts
                     if (this.bubbleSort_i < this.sortElements.length - this.bubbleSort_x) {
+                        //Klassisches tauschen, wenn die verglichenen Elemente .... sind.
                         if ( this.sortElements[this.bubbleSort_i-1].value > this.sortElements[this.bubbleSort_i].value) {
                             temp = this.sortElements[this.bubbleSort_i];
                             this.sortElements[this.bubbleSort_i] = this.sortElements[this.bubbleSort_i-1];
                             this.sortElements[this.bubbleSort_i-1] = temp;
+                            // Animationsmanager mit der Bewegungsanimation beauftragen
                             this.animator.displace(this.bubbleSort_i - 1, {X: this.sortElements[this.bubbleSort_i].location.X, Y: this.sortElements[this.bubbleSort_i].location.Y});
                             this.animator.displace(this.bubbleSort_i, {X: this.sortElements[this.bubbleSort_i-1].location.X, Y: this.sortElements[this.bubbleSort_i-1].location.Y});    
-
+                            // Die nächsten n Updatedurchläufe den Algorithmus nicht starten
                             this.nextSteppIn = this.SORT_TIME;
                         } else {
                             this.bubbleSort_i++;
                         }
-                    } else if (this.bubbleSort_i >= this.sortElements.length) {
+                    } else if (this.bubbleSort_i >= this.sortElements.length - this.bubbleSort_x) {
+                        // Innere Schleife resetten
                         this.bubbleSort_i = 1;
+                        // Außere Schleife weiterzählen
                         this.bubbleSort_x++;
                     }
                 } else {
+                    // Äußere Schleife ist fertig
+                    // Alle Variablen resetten und für nächsten Sort bereit machen
                     this.isBubblesorting = false;
                     this.bubbleSort_i = 1;
                     this.bubbleSort_x = 0;
-                    this.SORT_TIME =  40;
                     this.nextSteppIn = 0;
                 }
             } else {
